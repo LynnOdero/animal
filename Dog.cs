a24a37ff6d2fd7a75e779b59576991a4bd1cc676
Dro <davidrogola@gmail.com>
2020-02-24 20:50:04 +0300

    Initial commit for animal test project

---
+++ Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    class Dog
+    {
+        public string Breed { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+        public bool AvailableForAdoption { get; set; }
+
+    }
+}
+
+       
+
+       
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.Breed = "German Shepherd";
+
+            d.Color = "Black White";
+            d.Name = "Zuki";
+            d.Age = 6;
+            d.AvailableForAdoption = true;
+         
+            
+
+            Console.WriteLine("About Dog: Breed: {0} Color: {1} Name: {2} Age: {3}", d.Breed, d.Color, d.Name, d.Age);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
